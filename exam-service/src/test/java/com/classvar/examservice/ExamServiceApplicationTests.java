f04fe786e681ea7c2bbdc8d8c0339cd4ac7611d2
kim hyun sik <devconf5296@gmail.com>
2021-08-02 20:44:47 +0900

    Add exam service template

---
+++ exam-service/src/test/java/com/classvar/examservice/ExamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classvar.examservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
