3f3331e72ae2157c402bc31b276f6a27a9b746c8
kim hyun sik <devconf5296@gmail.com>
2021-10-26 20:20:59 +0900

    feat:Add findCourseWithExamsById method into repository

--- src/main/java/com/classvar/course/infra/JpaCourseRepository.java
+++ src/main/java/com/classvar/course/infra/JpaCourseRepository.java
@@ -9,10 +9,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.Set;
 
 @Repository
 public interface JpaCourseRepository extends JpaRepository<Course, Long>, CourseRepository {
+ @Query("select c,e from Course c left join Exam e on e.course.id =:courseId")
+  Optional<Course> findCourseWithExamsById(@Param("courseId") Long courseId);
 
   @Query("select c.exams from Course c where c.id =:courseId")
   Set<Exam> findAllExamWithCourseId(@Param("courseId") Long courseId);
