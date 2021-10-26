55df04ef5d2e5e0ce807c7b0981e4a96d8f32427
enrouteeee <enrouteeee@gmail.com>
2021-10-26 16:23:53 +0900

    feat : add student domain layer

--- src/main/java/com/classvar/course/domain/Course.java
+++ src/main/java/com/classvar/course/domain/Course.java
@@ -25,6 +25,9 @@ public class Course {
       orphanRemoval = true)
   private Set<Exam> exams = new HashSet<>();
 
+  @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<Student> students = new HashSet<>();
+
   public Course(String name) {
     this.name = name;
   }
@@ -43,6 +46,25 @@ public class Course {
   public void deleteExam(long examId) {
     if (!this.exams.removeIf(exam -> exam.getId() == examId)) {
       throw new IllegalArgumentException("존재하지 않는 시험 입니다.");
+    }
+  }
+
+  public void addStudent(Student student){
+    student.setCourse(this);
+    this.students.add(student);
+  }
+
+  public void approveStudent(long studentId){
+    for (Student student : students) {
+      if(student.getId() == studentId){
+        student.isVerified();
+      }
+    }
+  }
+
+  public void deleteStudent(long studentId) {
+    if (!this.students.removeIf(student -> student.getId() == studentId)) {
+      throw new IllegalArgumentException("존재하지 않는 응시자 입니다.");
     }
   }
 }
