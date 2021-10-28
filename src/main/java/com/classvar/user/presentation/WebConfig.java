2772e70ed83b1424497d2ae41f2e5cd5f7fcd94f
enrouteeee <enrouteeee@gmail.com>
2021-10-28 13:35:38 +0900

    feat : add argumentResolver for loginUser

--- src/main/java/com/classvar/user/presentation/WebConfig.java
+++ src/main/java/com/classvar/user/presentation/WebConfig.java
@@ -1,9 +1,13 @@
 package com.classvar.user.presentation;
 
+import com.classvar.user.presentation.argumentresolver.LoginUserArgumentResolver;
 import com.classvar.user.presentation.interceptor.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -15,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns(
                         "/login"
                 );
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginUserArgumentResolver());
     }
 }
