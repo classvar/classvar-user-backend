3999214152ea3321cf2e1a079838ef4b1e53cb44
kim hyun sik <devconf5296@gmail.com>
2021-10-29 15:54:27 +0900

    Feat: Add regex to CreateOrUpdateUserDto and Implement UserCommandController

---
+++ src/main/java/com/classvar/user/application/dto/CreateOrUpdateUserDto.java
@@ -0,0 +1,27 @@
+package com.classvar.user.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateOrUpdateUserDto {
+
+  @NotEmpty(message = "email 필드는 비어 있을 수 없습니다.")
+  private String email;
+
+  @NotEmpty
+  @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$")
+  private String password;
+
+  @NotEmpty(message = "이름은 비어 있을 수 없습니다.")
+  private String name;
+
+  @NotEmpty(message = "department 필드는 비어 있을 수 없습니다.")
+  private String department;
+}
--- src/main/java/com/classvar/user/presentation/controller/UserCommandController.java
+++ src/main/java/com/classvar/user/presentation/controller/UserCommandController.java
@@ -1,48 +1,53 @@
 package com.classvar.user.presentation.controller;
 
 import com.classvar.user.application.UserCommandExecutor;
+import com.classvar.user.application.dto.CreateOrUpdateUserDto;
 import com.classvar.user.application.dto.LoginDto;
 import com.classvar.user.presentation.SessionConst;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
-@RestController
+@Controller
+@RequestMapping("/api")
 @RequiredArgsConstructor
-@RequestMapping(value = "/api")
 public class UserCommandController {
 
-    private final UserCommandExecutor userCommandExecutor;
+  private final UserCommandExecutor userCommandExecutor;
 
-    @PostMapping(value = "/login")
-    public ResponseEntity login(@Valid @RequestBody LoginDto dto, HttpServletRequest request){
+  @PostMapping(value = "/signUp")
+  public ResponseEntity userSignUp(@Valid @RequestBody CreateOrUpdateUserDto dto) {
+    userCommandExecutor.createUser(dto);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
 
-        Long loginId = userCommandExecutor.login(dto);
+  @PostMapping(value = "/login")
+  public ResponseEntity login(@Valid @RequestBody LoginDto dto, HttpServletRequest request) {
 
-        //login fail
-        if(loginId == null){
+    Long loginId = userCommandExecutor.login(dto);
 
-        }
+    // login fail
+    if (loginId == null) {}
 
-        HttpSession session = request.getSession();
-        session.setAttribute(SessionConst.LOGIN_ID, loginId);
-        return ResponseEntity.status(HttpStatus.OK).build();
+    HttpSession session = request.getSession();
+    session.setAttribute(SessionConst.LOGIN_ID, loginId);
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
+
+  @PostMapping(value = "/logout")
+  public ResponseEntity logout(HttpServletRequest request) {
+    HttpSession session = request.getSession(false);
+    if (session != null) {
+      session.invalidate();
     }
-
-    @PostMapping(value = "/logout")
-    public ResponseEntity logout(HttpServletRequest request){
-        HttpSession session = request.getSession(false);
-        if(session != null){
-            session.invalidate();
-        }
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
+    return ResponseEntity.status(HttpStatus.OK).build();
+  }
 }
