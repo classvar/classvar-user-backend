505e152340b666a569c43bb1f1008c81b04f934b
kim hyun sik <devconf5296@gmail.com>
2021-10-29 15:48:28 +0900

    Fix: Change EntityMapper name

--- src/main/java/com/classvar/user/application/UserCommandExecutor.java
+++ src/main/java/com/classvar/user/application/UserCommandExecutor.java
@@ -1,25 +1,38 @@
 package com.classvar.user.application;
 
+import com.classvar.user.application.common.UserMapper;
+import com.classvar.user.application.dto.CreateOrUpdateUserDto;
 import com.classvar.user.application.dto.LoginDto;
 import com.classvar.user.domain.User;
 import com.classvar.user.domain.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 public class UserCommandExecutor {
 
-    private final UserRepository userRepository;
+  private UserRepository userRepository;
+  private UserMapper userMapper;
 
-    @Transactional
-    public Long login(LoginDto dto){
-        User findUser = userRepository.findByEmail(dto.getEmail())
-                .filter(u -> u.getPassword().equals(dto.getPassword()))
-                .orElse(null);
-        if(findUser == null)
-            return null;
-        return findUser.getId();
-    }
+  public UserCommandExecutor(UserRepository userRepository, UserMapper userMapper) {
+    this.userRepository = userRepository;
+    this.userMapper = userMapper;
+  }
+
+  public long createUser(CreateOrUpdateUserDto dto) {
+    User user = userMapper.toUser(dto);
+    userRepository.save(user);
+    return user.getId();
+  }
+
+  @Transactional
+  public Long login(LoginDto dto) {
+    User findUser =
+        userRepository
+            .findByEmail(dto.getEmail())
+            .filter(u -> u.getPassword().equals(dto.getPassword()))
+            .orElse(null);
+    if (findUser == null) return null;
+    return findUser.getId();
+  }
 }
