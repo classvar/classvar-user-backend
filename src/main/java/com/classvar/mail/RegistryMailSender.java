f419cd8bfbec37725dc51d36c07fde1f8113fdff
enrouteeee <enrouteeee@gmail.com>
2021-11-25 22:17:23 +0900

    feat : add sendRegistryMailToMangager on RegistryMailSender

--- src/main/java/com/classvar/mail/RegistryMailSender.java
+++ src/main/java/com/classvar/mail/RegistryMailSender.java
@@ -1,5 +1,7 @@
 package com.classvar.mail;
 
+import com.classvar.manager.domain.Manager;
+import com.classvar.manager.domain.ManagerCreatedEvent;
 import com.classvar.student.domain.Student;
 import com.classvar.student.domain.StudentCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -20,16 +22,31 @@ public class RegistryMailSender {
     private final String FROM_ADDRESS = "registry@classvar.com";
 
     @TransactionalEventListener
-    public void sendRegistryMailToStudent(StudentCreatedEvent createdStudentEvent) {
-        Student student = createdStudentEvent.getStudent();
+    public void sendRegistryMailToStudent(StudentCreatedEvent studentCreatedEvent) {
+        Student student = studentCreatedEvent.getStudent();
 
         String title = "ClassVar 응시자 등록 메일입니다.";
-        String registryUrl = "classvar.com/students/registry/" + student.getUuid();
+        String registryUrl = "http://classvar.com/students/registry/" + student.getUuid();
         String message = "안녕하세요 응시자 등록 서비스 입니다.\n" +
                 "링크에 접속해서 응시자 정보를 등록해주시기 바랍니다.\n" +
                 registryUrl;
 
         SimpleMailForm simpleMailForm = new SimpleMailForm(student.getEmail(), FROM_ADDRESS, title, message);
+
+        mailService.sendMail(simpleMailForm);
+    }
+
+    @TransactionalEventListener
+    public void sendRegistryMailToManager(ManagerCreatedEvent managerCreatedEvent) {
+        Manager manager = managerCreatedEvent.getManager();
+
+        String title = "ClassVar 감독관 등록 메일입니다.";
+        String registryUrl = "http://classvar.com/managers/registry/" + manager.getUuid();
+        String message = "안녕하세요 감독관 등록 서비스 입니다.\n" +
+                "링크에 접속해서 감독관 정보를 등록해주시기 바랍니다.\n" +
+                registryUrl;
+
+        SimpleMailForm simpleMailForm = new SimpleMailForm(manager.getEmail(), FROM_ADDRESS, title, message);
 
         mailService.sendMail(simpleMailForm);
     }
