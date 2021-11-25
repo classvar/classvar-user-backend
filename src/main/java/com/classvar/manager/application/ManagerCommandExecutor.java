f2ea0aa12bb89e289196ce070cd8fd6130b1396f
enrouteeee <enrouteeee@gmail.com>
2021-11-25 22:16:37 +0900

    feat : add ManagerCreatedEvenet

--- src/main/java/com/classvar/manager/application/ManagerCommandExecutor.java
+++ src/main/java/com/classvar/manager/application/ManagerCommandExecutor.java
@@ -6,10 +6,14 @@ import com.classvar.manager.application.dto.request.CreateManagerDto;
 import com.classvar.manager.application.dto.request.DeleteManagerDto;
 import com.classvar.manager.application.dto.request.UpdateManagerInfoDto;
 import com.classvar.manager.domain.Manager;
+import com.classvar.manager.domain.ManagerCreatedEvent;
 import com.classvar.manager.domain.ManagerRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,10 +21,16 @@ public class ManagerCommandExecutor {
 
   private final ManagerRepository managerRepository;
   private final ManagerMapper managerMapper;
+  private final ApplicationEventPublisher eventPublisher;
 
   @Transactional
-  public void createManager(CreateManagerDto dto) {
-    managerMapper.toManagers(dto).forEach(managerRepository::save);
+  public void createManager(CreateManagerDto dto){
+    List<Manager> managers = managerMapper.toManagers(dto);
+    for (Manager manager : managers) {
+      managerRepository.save(manager);
+
+      eventPublisher.publishEvent(new ManagerCreatedEvent(manager));
+    }
   }
 
   public void updateManager(String uuid, UpdateManagerInfoDto dto) {
